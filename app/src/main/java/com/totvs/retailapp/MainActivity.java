a744e3c819d6e5a6ce41b4509d882601761126ed
4talesa <rondinellifortalesa@gmail.com>
2015-11-20 10:29:48 -0800

    Square removed from browsing activities; Add timer to start the welcome activity from main;

--- app/src/main/java/com/totvs/retailapp/MainActivity.java
+++ app/src/main/java/com/totvs/retailapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.totvs.retailapp;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent it = new Intent(MainActivity.this, WelcomeActivity.class);
+
+                MainActivity.this.startActivity(it);
+            }
+
+        }, 1000L);
 
         TextView textViewMainTitle = (TextView)findViewById(R.id.textViewMainTitle);
 
