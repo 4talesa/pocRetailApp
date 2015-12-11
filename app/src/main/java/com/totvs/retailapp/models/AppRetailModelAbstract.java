82ac47548575e90e4d0e2ad0bca91856bbea6867
4talesa <rondinellifortalesa@gmail.com>
2015-12-11 12:02:26 -0800

    Fields postdate,putdate,deletedate added to AppRetailModelAbstract;

--- app/src/main/java/com/totvs/retailapp/models/AppRetailModelAbstract.java
+++ app/src/main/java/com/totvs/retailapp/models/AppRetailModelAbstract.java
@@ -1,5 +1,9 @@
 package com.totvs.retailapp.models;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Date;
+
 /**
  * Created by rond.borges on 09/12/2015.
  */
@@ -7,6 +11,9 @@ public abstract class AppRetailModelAbstract {
 
     protected String id;
     protected String _id;
+    @SerializedName("postdate") protected Date postDate;
+    @SerializedName("putdate") protected Date putDate;
+    @SerializedName("deletedate") protected Date deleteDate;
 
     public String getId(){
         return this.id;
@@ -22,6 +29,30 @@ public abstract class AppRetailModelAbstract {
 
     public void set_id(String _id){
         this._id = _id;
+    }
+
+    public Date getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(Date postDate) {
+        this.postDate = postDate;
+    }
+
+    public Date getPutDate() {
+        return putDate;
+    }
+
+    public void setPutDate(Date putDate) {
+        this.putDate = putDate;
+    }
+
+    public Date getDeleteDate() {
+        return deleteDate;
+    }
+
+    public void setDeleteDate(Date deleteDate) {
+        this.deleteDate = deleteDate;
     }
 
 }
