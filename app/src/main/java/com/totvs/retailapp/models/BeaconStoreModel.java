70112a7c20f7a756db202c5a69cb005d7b1e23dd
4talesa <rondinellifortalesa@gmail.com>
2015-12-11 16:35:35 -0800

    BeaconStoreModel added;

---
+++ app/src/main/java/com/totvs/retailapp/models/BeaconStoreModel.java
@@ -0,0 +1,33 @@
+package com.totvs.retailapp.models;
+
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+/**
+ * Created by rond.borges on 11/12/2015.
+ */
+public class BeaconStoreModel extends AppRetailModelAbstract {
+
+    private String idStore;
+
+    public String getIdStore(){
+        return idStore;
+    }
+
+    public void setIdStore(String idStore){
+        this.idStore = idStore;
+    }
+
+    public BeaconStoreModel(String id, String idStore){
+        this.id             = id;
+        this.idStore        = idStore;
+    }
+
+    static public BeaconStoreModel fromJson(JSONObject object) {
+        Gson gson = new Gson();
+
+        return gson.fromJson(object.toString(), BeaconStoreModel.class);
+    }
+
+}
